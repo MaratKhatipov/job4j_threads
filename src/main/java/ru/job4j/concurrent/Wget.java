da30a79c6abb23ef85182e83450e6837cf7b95c1
MaratKhatipov <mmmarat86@gmail.com>
2022-07-18 19:47:33 +0500

    1.2. Режим ожидания. [#231217] v0.1

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) throws InterruptedException {
+        Thread load = new Thread(
+                () -> {
+                    for (int i = 0; i <= 100; i++) {
+                        System.out.print("\rLoading : " + i + " % ");
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        load.start();
+        load.join();
+        System.out.println("\nDownload completed");
+    }
+}
