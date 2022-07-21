6c4fb97c28c2604ce39f6046baa03fef8b0bdf9e
MaratKhatipov <mmmarat86@gmail.com>
2022-07-21 18:10:32 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102] v0.1

---
+++ src/main/java/ru/job4j/concurrent/GetContent.java
@@ -0,0 +1,45 @@
+package ru.job4j.concurrent;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.function.Predicate;
+
+public final class GetContent {
+
+    private final File file;
+
+    public GetContent(File file) {
+        this.file = new File(file.toString());
+    }
+
+    public String predicateContent(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        synchronized (this) {
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                int data;
+                char charData;
+                while ((data = bis.read()) != 1) {
+                    charData = (char) data;
+                    if (filter.test(charData)) {
+                        output.append(charData);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return output.toString();
+        }
+    }
+
+    public String getContent() {
+        return predicateContent(data -> true);
+    }
+
+    public String getContentWithoutUnicode() {
+        return predicateContent(data -> data < 0x80);
+    }
+}
+
+
---
+++ src/main/java/ru/job4j/concurrent/SaveContent.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+
+public final class SaveContent {
+    private final File file;
+
+    public SaveContent(File file) {
+        this.file = new File(file.toString());
+    }
+
+    public synchronized void saveContent(String content) {
+       try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+           for (int i = 0; i < content.length(); i += 1) {
+               o.write(content.charAt(i));
+           }
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+    }
+}
