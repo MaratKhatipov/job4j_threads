313a1b8344f6d60138ca2b4e78589e717254a5d1
MaratKhatipov <mmmarat86@gmail.com>
2022-07-23 17:08:04 +0500

    0. Управление нитью через wait. [#6858] v0.1

---
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -0,0 +1,58 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class CountBarrier {
+    @GuardedBy("monitor")
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            System.out.println("count in method - " + count);
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    System.out.println("count - " + count + " total - " + total);
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountBarrier test = new CountBarrier(5);
+        Thread first = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                    test.count();
+                }, "count method"
+        );
+
+        Thread second = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName());
+                    test.await();
+                }, "AWAIT method"
+        );
+        first.start();
+        second.start();
+    }
+}
