9e92a50758a6573bc78d74c72b19b3ea9cdead4f
MaratKhatipov <mmmarat86@gmail.com>
2022-07-25 15:34:21 +0500

    2. Обеспечить остановку потребителя. [#66825 #325391]v0.3

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -4,22 +4,21 @@ import ru.job4j.SimpleBlockingQueue;
 
 public class ParallelSearch {
     public static void main(String[] args) throws InterruptedException {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(4);
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
         final Thread consumer = new Thread(
                 () -> {
                     while (!Thread.currentThread().isInterrupted()) {
                         try {
                             System.out.println(queue.poll());
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            System.out.println("consumer остановлен с ошибкой - " + e);
                             Thread.currentThread().interrupt();
                         }
                     }
-                    System.out.println("consumer is FINISHED");
                 }
         );
-
-        final Thread producer = new Thread(
+        consumer.start();
+        new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         try {
@@ -31,17 +30,11 @@ public class ParallelSearch {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
-                            Thread.currentThread().interrupt();
                         }
+                        consumer.interrupt();
                     }
-
                 }
-        );
-        producer.start();
-        consumer.start();
-        producer.join();
-        consumer.interrupt();
-        consumer.join();
-        System.out.println("DONE !");
+
+        ).start();
     }
 }
