7674bffa8a15853aa87c6a5d0a097a3922908620
MaratKhatipov <mmmarat86@gmail.com>
2022-07-27 10:38:12 +0500

    1. Неблокирующий кеш [#4741] v0.1

---
+++ src/main/java/ru/job4j/cas/cache/Cache.java
@@ -0,0 +1,31 @@
+package ru.job4j.cas.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        BiFunction<Integer, Base, Base> updateFunction = (id, base) -> {
+            Base stored = memory.get(model.getId());
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            Base newModel = new Base(id, model.getVersion() + 1);
+            newModel.setName(model.getName());
+            return newModel;
+        };
+        memory.computeIfPresent(model.getId(), updateFunction);
+        return false;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+}
