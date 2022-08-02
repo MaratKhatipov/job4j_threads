8f4eeeac54a78b1940cb30948ad0d7c17fa743dc
MaratKhatipov <mmmarat86@gmail.com>
2022-08-02 15:14:02 +0500

    378. Kth The Smallest Element in a Sorted Matrix. Optimized

--- src/main/java/ru/job4j/example/leetcode/KthSmallest378.java
+++ src/main/java/ru/job4j/example/leetcode/KthSmallest378.java
@@ -1,18 +1,27 @@
 package ru.job4j.example.leetcode;
 
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class KthSmallest378 {
     public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
         int n = matrix.length;
-        int[] res = new int[n * n];
-        int index = 0;
-        for (int[] ints : matrix) {
+
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                res[index++] = ints[j];
+                if (pq.size() < k) {
+                    pq.add(matrix[i][j]);
+                } else { //equal to k
+                    if (matrix[i][j] < pq.peek()) {
+                        pq.poll();
+                        pq.add(matrix[i][j]);
+                    }
+                }
             }
         }
-        Arrays.sort(res);
-        return res[k - 1];
+
+        return pq.peek();
     }
 }
